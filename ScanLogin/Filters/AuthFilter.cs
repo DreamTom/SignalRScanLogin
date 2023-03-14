f18e01782845914a48f9b6a24a2bfa67d12a4099
xwt <vintom@qq.com>
2023-03-15 00:07:50 +0800

    add unfished update

--- ScanLogin/Filters/AuthFilter.cs
+++ ScanLogin/Filters/AuthFilter.cs
@@ -1,6 +1,16 @@
-﻿namespace ScanLogin.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ScanLogin.Filters
 {
-    public class AuthFilter
+    public class AuthFilter : IAsyncAuthorizationFilter
     {
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            bool hasToken = context.HttpContext.Request.Headers.TryGetValue("token", out var token);
+            if (hasToken)
+            {
+
+            }
+        }
     }
 }
